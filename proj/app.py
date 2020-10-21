41f7e46155087b77b151692086b102e562e59e4a
Ty <tycodes@protonmail.com>
2020-10-21 18:31:39 -0500

    Added the login form to its own page; on the apply page, consolidated the personal and account info into one form

--- proj/app.py
+++ proj/app.py
@@ -16,16 +16,72 @@ con = sqlite3.connect('data.db', check_same_thread=False)
 
 #Homepage
 @app.route('/')
-@app.route('/home')
+#@app.route('/home/')
 def home():
 
     #msg = "Hello World!"
-    return render_template('index.html') #,msg=msg)
+    return render_template('home.html') #,msg=msg)
+
+
 
 #Apply section of webpage
-@app.route('/apply')
+@app.route('/apply/', methods=['POST', 'GET'])
 def apply():
-	return render_template('apply.html')
+    msg = ""
+
+    print("gets to apply")
+    # User creates an account
+    if request.method == 'POST':
+
+        print("gets to apply")
+
+        # Get the form data
+        fn_enter = request.form['fname']
+        ln_enter = request.form['lname']
+        phone_enter = request.form['phone']
+        em_enter = request.form['email']
+        pw_enter = request.form['pass']
+        pwCon_enter = request.form['conPass']
+
+        try:
+            if pw_enter == pwCon_enter:
+                msg = "Password confirmed"
+                print(msg)
+                return render_template('apply.html', msg=msg)
+            else:
+                msg = "Passwords do not match"
+                print(msg)
+                return render_template('apply.html', msg=msg)
+
+        except:
+            msg = "Invalid form. Try again."
+            return render_template('apply.html', msg=msg)
+
+
+    return render_template('apply.html', msg=msg)
+
+
+# Login page
+@app.route('/login/', methods=['POST', 'GET'])
+def login():
+    msg = ""
+
+    # User login attempt
+    if request.method == 'POST':
+
+        em_enter = request.form['email']
+        pw_enter = request.form['pass']
+
+        msg = "Login attempt with email: " + em_enter + " and password: " + pw_enter
+
+
+    return render_template('login.html', msg=msg)
+
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run()
